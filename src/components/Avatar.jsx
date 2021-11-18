eb80bd2147a047ad26b2ce79bc74865fc4c534eb
yellyoshua <usuario1529yoshua@hotmail.com>
2021-11-17 21:59:32 -0500

    avatar correction

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -8,7 +8,7 @@ export default function Avatar() {
   return (
     <nav className="bg-transparent justify-content-center align-items-center navbar navbar-light bg-light">
       <p style={styleUsername} className={app.user ? 'text-white d-flex align-items-center navbar-brand' : 'm-auto text-white d-flex align-items-center navbar-brand'}>
-        <img onClick={actions.removeAvatar} src={app.avatar} width="40" height="40" className="d-inline-block align-top mx-3" alt="" />
+        {app.avatar && <img onClick={actions.removeAvatar} src={app.avatar} width="40" height="40" className="d-inline-block align-top mx-3" alt="app-user-avatar" />}
         {app.user}
       </p>
     </nav>
