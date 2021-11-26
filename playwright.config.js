a3977aef1d027e3969ac504bd083b7622fab077d
yellyoshua <usuario1529yoshua@hotmail.com>
2021-11-25 21:12:24 -0500

    ci e2e config 7

--- playwright.config.js
+++ playwright.config.js
@@ -15,20 +15,6 @@ const config = {
 	use: {
 		trace: 'on-first-retry',
 	},
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] },
-		},
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] },
-		},
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] },
-		},
-	],
 };
 
 module.exports = config;
