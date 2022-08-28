6d5040a5621f69be7fdcadeee650f729302bb073
yellyoshua <usuario1529yoshua@hotmail.com>
2022-08-28 13:57:24 -0500

    update graphql config

--- src/api/config.js
+++ src/api/config.js
@@ -2,6 +2,8 @@ import { GraphQLClient } from 'graphql-request';
 
 export const GetGraphqlClient = () => {
 	return new GraphQLClient(String(process.env.REACT_APP_API_URI), {
-		headers: { Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}` },
+		headers: {
+			Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`
+		},
 	});
 };
