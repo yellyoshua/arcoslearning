43026a99f36d27843d486badf7d55b88547885ae
yellyoshua <usuario1529yoshua@hotmail.com>
2024-01-14 02:46:05 -0500

    Add assignments page

--- client-v2/src/hooks/useAssignmentsStore.tsx
+++ client-v2/src/hooks/useAssignmentsStore.tsx
@@ -4,10 +4,12 @@ import { create } from "zustand";
 
 type AssignmentsStore = {
 	assignments: any[];
+	assignment?: any;
 };
 
 export default create<AssignmentsStore>(
 	(set, get) => ({
 		assignments: [],
+		assignment: null,
 	})
 );
--- client-v2/src/pages/HomePage.tsx
+++ client-v2/src/pages/HomePage.tsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
+import { Link } from "wouter";
 import Footer from "../components/Footer";
 import NavigationHeader from "../components/NavigationHeader";
+import useAssignmentsStore from "../hooks/useAssignmentsStore";
 
 export default function HomePage() {
+	const assignments = useAssignmentsStore((state) => state.assignments);
+	const [query, setQuery] = useState(() => {
+		const query = new URLSearchParams(window.location.search);
+		return Object.fromEntries(query);
+	});
+
 	return (
-		<div className="max-w-4xl grid grid-rows-3 gap-6 h-screen">
+		<div className="max-w-4xl grid gap-6">
 			<NavigationHeader/>
 
 			<p className="text-center text-xl text-cyan-500" style={{textShadow: '1px 1px #7b8081'}}>
-				Here we go!
+				Selecciona con que quieres comenzar
 			</p>
+
+			<div className="grid grid-cols-2">
+				{
+					assignments.map((assignment) => (
+						<Link to={`/?assignment=${assignment.id}`} onClick={() => setQuery(state => ({...state, assignments: assignment.id}))} key={`${assignment.name}`}>
+							<div className=" text-slate-600 hover:text-white transition-colors bg-white hover:bg-slate-700 p-3 m-3 rounded-xl cursor-pointer">
+								<div className='card-body text-center m-auto'>
+									<h3 className='card-text text-danger'>{assignment.name}</h3>
+								</div>
+							</div>
+						</Link>
+					))
+				}
+			</div>
+
+			{!assignments.length && (
+				<div className="text-center text-xl text-slate-600 my-20" style={{textShadow: '1px 1px #7b8081'}}>
+					No hay asignaturas disponibles
+				</div>
+			)}
+
 			<Footer />
 		</div>
 	);
